fb2dd181ccdfdac3187ee83b847a63e4feeca6d1
Andy Weaver <andrewayersweaver@gmail.com>
2024-10-18 03:37:20 -0400

    moving maybe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use crabby::{ModelData, RealMatrix};
+use rand::Rng;
+
+fn generate_large_dataset(n: usize) -> Vec<f64> {
+    let mut rng = rand::thread_rng();
+    let mut data = Vec::new();
+    for _ in 0..n {
+        data.push(Rng::gen_range(&mut rng, 0.0, 1.0));
+    }
+    data
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = RealMatrix::from_vec(generate_large_dataset(1000000), 1000000, 1);
+    let y = RealMatrix::from_vec(&x.values.iter().map(|x| x * 2.0).collect::<Vec<f64>>());
+    let model_data = ModelData::new(&x, &y);
 }
